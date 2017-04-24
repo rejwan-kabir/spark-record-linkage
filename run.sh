44c3073f6e34bb1b54782c0314eb4e19019a9d9b
Rejwan Shamsul Kabir <rejwan.shuvo@gmail.com>
2017-04-24 18:56:09 +0600

    added sbt command to run.sh.

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-
 if [ $# -ne 2 ]; then
     echo "Usage: <command> <input-path> <output-path>"
     exit 1
 fi
-
+sbt assembly
 spark-submit --class spark.rl.Main target/scala-2.11/spark-rl.jar "$1" "$2"
-
 exit 0
