52efefa3b65bdf80db2e86f3abd6579df823e658
Rejwan Shamsul Kabir <rejwan.shuvo@gmail.com>
2017-05-14 12:12:21 +0600

    Minor refactoring

--- src/main/scala-2.11/spark/rl/Main.scala
+++ src/main/scala-2.11/spark/rl/Main.scala
@@ -7,8 +7,7 @@ import scala.reflect.io.Path
 import scala.util.Try
 
 object Main {
-  val spark = SparkSession
-    .builder()
+  val spark = SparkSession.builder()
     .appName("A simple Spark Record Linkage")
     .master("local")
     .getOrCreate()
@@ -24,34 +23,27 @@ object Main {
     }
     var inputDir = args(0)
     if (inputDir.endsWith("/")) {
-      inputDir = inputDir.substring(0, inputDir.length-1)
+      inputDir = inputDir.substring(0, inputDir.length - 1)
     }
-    var outputDir = args(1)
-    if (outputDir.endsWith("/")) {
-      outputDir = outputDir.substring(0, outputDir.length-1)
-    }
+    val outputDir = args(1)
     Try(Path(outputDir).deleteRecursively())
     val products = spark.read.json(inputDir + "/products.txt")
     val listings = spark.read.json(inputDir + "/listings.txt")
     val combined = products.as("p").join(listings.as("l"), $"p.manufacturer" <=> $"l.manufacturer")
       .groupBy($"p.manufacturer", $"p.model", $"p.family", $"l.currency", $"l.price")
       .agg(first($"p.product_name").as("product_name"), first($"l.title").as("title"))
-
     val without_family = combined.filter("family is null")
     without_family.registerTempTable("without_family")
     val matched_without_family = spark.sql("SELECT * FROM without_family WHERE CONCAT(' ', title, ' ') like CONCAT('% ', model, ' %')")
-
     val with_family = combined.filter("family is not null")
     with_family.registerTempTable("with_family")
     val matched_with_family = spark.sql("SELECT * FROM with_family WHERE CONCAT(' ', title, ' ') like CONCAT('% ', model, ' %') AND CONCAT(' ', title, ' ') like CONCAT('% ', family, ' %')")
-
     val all_matched = matched_with_family.unionAll(matched_without_family)
     val pair = all_matched.rdd.map({
       case Row(manufacturer: String, _, _, currency: String, price: String, product_name: String, title: String) =>
         (product_name, List(Listing(title, manufacturer, currency, price)))
     }).foldByKey(List())((acc, cur) => acc ++ cur)
       .toDF("product_name", "listings")
-
     pair.coalesce(1).write.json(outputDir)
     spark.stop()
   }
